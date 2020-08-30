9cfa9fbb6a2f0313be150f107028ee8447abe848
Tuc <tusty53@gmail.com>
2020-08-30 22:06:57 +0200

    Day 3 Task 2 - generated

--- day_4/task_1/main.py
+++ day_4/task_1/main.py
@@ -2,18 +2,28 @@ MIN_VAL = 256310
 MAX_VAL = 732736
 
 
-def check_pairs(x):
+def check_complicated_pairs(x):
     num_string = str(x)
-    for i in range(5):
-        if num_string[i] == num_string[i+1]:
-            return True
+    previous_digit = 0
+    same_digit_count = 0
+    for i in range(6):
+        current_digit = num_string[i]
+        if current_digit == previous_digit:
+            same_digit_count += 1
+        else:
+            previous_digit = current_digit
+            if same_digit_count == 2:
+                return True
+            same_digit_count = 1
+    if same_digit_count == 2:
+        return True
     return False
 
 
 def check_not_decreasing(x):
     num_string = str(x)
     for i in range(5):
-        if int(num_string[i]) > int(num_string[i+1]):
+        if int(num_string[i]) > int(num_string[i + 1]):
             return False
     return True
 
@@ -21,7 +31,7 @@ def check_not_decreasing(x):
 if __name__ == "__main__":
     number_of_good_passwords = 0
     for x in range(MIN_VAL, MAX_VAL):
-        if check_pairs(x) and check_not_decreasing(x):
+        if check_not_decreasing(x) and check_complicated_pairs(x):
             print(x)
             number_of_good_passwords += 1
     print(number_of_good_passwords)
